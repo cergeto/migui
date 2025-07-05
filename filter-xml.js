48b6db82295c340b1202791ffe3d99fc11ac50e8
cergeto <70757832+cergeto@users.noreply.github.com>
2025-07-05 21:45:20 +0200

    Update filter-xml.js

--- filter-xml.js
+++ filter-xml.js
@@ -103,11 +103,13 @@ function parseXML(xmlIconosData) {
       .filter(p => 
         [
           'La 1 HD', 'La 2', 'Antena 3 HD', 'Cuatro HD', 'Telecinco HD', 'La Sexta HD', 'TRECE', 'El Toro TV', 'Mega', 'DMAX', 
-          'DKISS', 'Ten', 'AMC Break', 'Discovery', 'Canal Hollywood HD', 'Atrescine', 'Somos', 'AMC HD', 'TCM HD', 'AXN Movies HD', 
-          'DARK', 'Sundance TV', 'Be Mad', 'XTRM', 'Paramount Network', 'Cine Feel Good', 'Runtime Cine y Series', 'Runtime Crimen', 
-          'Runtime Acción', 'Runtime Thriller-Terror', 'Runtime Comedia', 'RunTime Clásicos', 'Runtime Romance', 'RunTime Familia', 
-          'Pelis Top (Rakuten TV)', 'Acción (Rakuten TV)', 'Comedias (Rakuten TV)', 'Dramas (Rakuten TV)', 'Películas Románticas (Rakuten TV)', 
-          'Cine Español (Rakuten TV)', 'Thrillers (Rakuten TV)' 
+          'DKISS', 'Ten', 'AMC Break', 'Discovery', 'BBC Top Gear', 'Canal Hollywood HD', 'Atrescine', 'Somos', 'AMC HD', 'TCM HD', 
+          'AXN Movies HD', 'DARK', 'Sundance TV', 'Be Mad', 'XTRM', 'Paramount Network', 'Cine Feel Good', 'Runtime Cine y Series', 
+          'Runtime Crimen', 'Runtime Acción', 'Runtime Thriller-Terror', 'Runtime Comedia', 'RunTime Clásicos', 'Runtime Romance', 
+          'RunTime Familia', 'Pelis Top (Rakuten TV)', 'Acción (Rakuten TV)', 'Comedias (Rakuten TV)', 'Dramas (Rakuten TV)', 
+          'Películas Románticas (Rakuten TV)', 'Cine Español (Rakuten TV)', 'Thrillers (Rakuten TV)', 'En Familia (Rakuten TV)', 
+          'Energy', 'Divinity', 'Neox', 'Atreseries', 'Factoría de Ficción', 'Nova', 'Cosmo HD', 'STAR Channel HD', 'Warner TV HD', 
+          'Calle 13 HD', 'AXN HD', 'SyFy HD', 'BBC Drama'
         ].includes(p.$.channel));
 
     const programasXML = programasFiltrados.map(p => ({
