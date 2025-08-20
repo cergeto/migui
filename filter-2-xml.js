d6cec3d0e86424526c97bb87b686b7345fea1e26
cergeto <70757832+cergeto@users.noreply.github.com>
2025-08-20 11:10:22 +0200

    Update filter-2-xml.js

--- filter-2-xml.js
+++ filter-2-xml.js
@@ -110,7 +110,7 @@ async function fetchXMLFromSources() {
 
   // Canales que quieres permitir
   const canalesPermitidos = [
-    'Oficios perdidos.es', 'Canal Parlamento.es', 'Actualidad 360.es', 'France 24 ES.es', 'DW en español.es', 'La Abeja Maya.es',
+    'Oficios perdidos.es', 'Canal Parlamento.es', 'Actualidad 360.es', 'DW en español.es', 'La Abeja Maya.es',
     'tastemade-sp', 'cops-en-espanol',
     '608049aefa2b8ae93c2c3a63-67a1a8ef2358ef4dd5c3018e',
     'Atrescine.es', 'RTenEspanol.ru', 'France24.fr@Spanish', 'GaliciaTVAmerica.es' 
